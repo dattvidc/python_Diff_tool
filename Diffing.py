46084868c22f99c54c92e70c21affce624dcc208
david <d@vidc.dk>
2017-05-11 13:02:31 +0200

    files uploaded
    two list files and actual diffing pyfile added

---
+++ Diffing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+# Opens lista and listb respectively as read only
+A = open("lista.txt", "r")
+B = open("listb.txt", "r")
+
+listA = A.read().strip().splitlines()
+# listA = ['d01', '2', '3', '4']
+listB = B.read().strip().splitlines()
+# listB = ['7', '6', '3']
+# Result A - Only in A / Result B - Only in B / Result C - Both A and B/
+
+ResultA = list(set(listA) - set(listB))
+ResultB = list(set(listB) - set(listA))
+ResultC = list(set(listA) & set(listB))
+
+print(ResultA)
+print(ResultB)
+print(ResultC)
+# Closing both files
+A.close()
+B.close()
